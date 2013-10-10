tqrg-bot/qemu

msix: split msix_free from msix_uninit
Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

hw/virtio/virtio-pci.c
@@ -999,6 +999,7 @@ static void virtio_pci_exit(PCIDevice *pci_dev)
     virtio_pci_stop_ioeventfd(proxy);
     memory_region_destroy(&proxy->bar);
     msix_uninit_exclusive_bar(pci_dev);
+    msix_free(pci_dev);
 }
 
 static void virtio_pci_reset(DeviceState *qdev)

ncultra/qemu-1

hw/virtio/virtio-pci.c
@@ -998,6 +998,7 @@ static void virtio_pci_exit(PCIDevice *pci_dev)
     VirtIOPCIProxy *proxy = VIRTIO_PCI(pci_dev);
     virtio_pci_stop_ioeventfd(proxy);
     msix_uninit_exclusive_bar(pci_dev);
+    msix_free_exclusive_bar(pci_dev);
 }
 
 static void virtio_pci_instance_finalize(Object *obj)
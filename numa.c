2e60d39a3ed478bd18d980751cd057cd4112c5c4
Wanlong Gao <gaowanlong@cn.fujitsu.com>
2013-12-04 15:58:50 +0800

    NUMA: check if the total numa memory size is equal to ram_size
    If the total number of the assigned numa nodes memory is not
    equal to the assigned ram size, it will write the wrong data
    to ACPI talb, then the guest will ignore the wrong ACPI table
    and recognize all memory to one node. It's buggy, we should
    check it to ensure that we write the right data to ACPI table.
    
    Signed-off-by: Wanlong Gao <gaowanlong@cn.fujitsu.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- numa.c
+++ numa.c
@@ -127,6 +127,7 @@ void numa_add(const char *optarg)
 void set_numa_nodes(void)
 {
     if (nb_numa_nodes > 0) {
+        uint64_t numa_total;
         int i;
 
         if (nb_numa_nodes > MAX_NODES) {
@@ -152,6 +153,16 @@ void set_numa_nodes(void)
                 usedmem += node_mem[i];
             }
             node_mem[i] = ram_size - usedmem;
+        }
+
+        numa_total = 0;
+        for (i = 0; i < nb_numa_nodes; i++) {
+            numa_total += node_mem[i];
+        }
+        if (numa_total != ram_size) {
+            fprintf(stderr, "qemu: numa nodes total memory size "
+                            "should equal to ram_size\n");
+            exit(1);
         }
 
         for (i = 0; i < nb_numa_nodes; i++) {
